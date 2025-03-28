4f94b9be0e0d8e6e55a58d874f317a5d2a66b44d
sergio <chechocampos1987@gmail.com>
2025-03-28 15:35:53 -0500

    Redirigir ruta raíz al listado de feligreses

--- iglesia/urls.py
+++ iglesia/urls.py
@@ -1,8 +1,10 @@
+from django.shortcuts import redirect
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
+    path('', lambda request: redirect('feligreses/', permanent=False)),
     path('admin/', admin.site.urls),  # Admin de Django
     path('feligreses/', include('feligreses.urls')),  # URLs de feligreses
     path('logout/', LogoutView.as_view(next_page='/accounts/login/'), name='logout'),  # Logout redirige a login
