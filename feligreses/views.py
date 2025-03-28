6c29d08f046a791d013206848cef6eefe4d9e5cf
sergio <chechocampos1987@gmail.com>
2025-03-28 16:15:45 -0500

    Proteger vista principal con login_required

--- feligreses/views.py
+++ feligreses/views.py
@@ -1,44 +1,45 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Feligres
 from .forms import FeligresForm
 from django.contrib.auth.decorators import login_required
 
-# Vista principal: lista de iglesias protegida con login
 @login_required
-def lista_iglesias(request):
-    iglesias = Feligres.objects.all()
-    return render(request, 'feligreses/lista_iglesias.html', {'iglesias': iglesias})
+def lista_feligreses(request):
+    feligreses = Feligres.objects.all()
+    return render(request, 'feligreses/lista.html', {'feligreses': feligreses})
 
-# Añadir feligrés
+@login_required
+def detalle_feligres(request, feligres_id):
+    feligres = get_object_or_404(Feligres, id=feligres_id)
+    return render(request, 'feligreses/detalle.html', {'feligres': feligres})
+
 @login_required
 def agregar_feligres(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = FeligresForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('lista_iglesias')
+            return redirect('lista_feligreses')
     else:
         form = FeligresForm()
     return render(request, 'feligreses/agregar.html', {'form': form})
 
-# Editar feligrés
 @login_required
-def editar_feligres(request, pk):
-    feligres = get_object_or_404(Feligres, pk=pk)
-    if request.method == 'POST':
+def editar_feligres(request, feligres_id):
+    feligres = get_object_or_404(Feligres, id=feligres_id)
+    if request.method == "POST":
         form = FeligresForm(request.POST, instance=feligres)
         if form.is_valid():
             form.save()
-            return redirect('lista_iglesias')
+            return redirect('lista_feligreses')
     else:
         form = FeligresForm(instance=feligres)
-    return render(request, 'feligreses/editar.html', {'form': form})
+    return render(request, 'feligreses/editar.html', {'form': form, 'feligres': feligres})
 
-# Eliminar feligrés
 @login_required
-def eliminar_feligres(request, pk):
-    feligres = get_object_or_404(Feligres, pk=pk)
-    if request.method == 'POST':
+def eliminar_feligres(request, feligres_id):
+    feligres = get_object_or_404(Feligres, id=feligres_id)
+    if request.method == "POST":
         feligres.delete()
-        return redirect('lista_iglesias')
+        return redirect('lista_feligreses')
     return render(request, 'feligreses/eliminar.html', {'feligres': feligres})
